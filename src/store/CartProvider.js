fc5779418e9394cdb0e9512cc2e66714ff58cd91
dzoliana <dzolicana@gmail.com>
2023-07-14 11:25:28 +0200

    Adding useEffect hook for button bumping

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,23 +10,41 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    console.log(updatedTotalAmount);
+    //22.99 prva cijena
+    //45.98 cijena dva sushija
 
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
+    console.log(existingCartItemIndex);
+    //-1 kada kliknemo za prvi sushi (trenutno ne postoji nijedan)
+    //0 dodan još jedan sushi (jedan već postoji u košarici)
 
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
+    console.log(existingCartItem);
+    //undefinied
+    //{id: 'm1', name: 'Sushi', amount: 1, price: 22.99}...
 
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
         amount: existingCartItem.amount + action.item.amount,
       };
+      console.log(updatedItem);
+      //{id: 'm1', name: 'Sushi', amount: 2, price: 22.99}
+
       updatedItems = [...state.items];
+      console.log(updatedItems);
+      //0: {id: 'm1', name: 'Sushi', amount: 2, price: 22.99}
+
       updatedItems[existingCartItemIndex] = updatedItem;
+      console.log(updatedItems);
+      //0: {id: 'm1', name: 'Sushi', amount: 2, price: 22.99}
     } else {
       updatedItems = state.items.concat(action.item);
+      //ili  updatedItems = [...state.items, action.item];
     }
 
     return {
@@ -34,6 +52,28 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
